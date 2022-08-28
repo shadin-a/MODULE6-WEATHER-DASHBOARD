ff4068aa588bc39360abfc32483637801b6ca9c1
Shadin Al-Arab <shadin.alarab@gmail.com>
2022-08-28 10:54:16 -0700

    weather api calls functional

--- script.js
+++ script.js
@@ -1,28 +1,36 @@
-const TEMP_CITY = "Kirkland"; // Replace me later with user input from form
+const TEMP_CITY = "KIRKLAND"; // Replace me later with user input from form
 const API_KEY = "c2647d1123d8569a6d9fce9c4ea94398";
 const CURRENT_WEATHER_ENDPOINT = 'https://api.openweathermap.org/data/2.5/weather?';
-const FORECAST_ENDPOINT = 'api.openweathermap.org/data/2.5/forecast?'
+const FORECAST_ENDPOINT = 'https://api.openweathermap.org/data/2.5/forecast?'
+const SEARCH_BUTTON = document.getElementById("searchButton");
 
 // 1. Translate the city into latitude and longitude
 // 2. Send lat and long to api to get weather data back
 
-function onClickSubmit() {
-    var cityName = TEMP_CITY; // plaeholder for getting data from form
-    var latAndLong = getLatAndLongFromCityName(cityName);
-    var weatherData = getWeatherDataFromApi(lat, long);
-}
+$("#searchButton").click(function(event){
+    event.preventDefault();
+    var cityName = TEMP_CITY.val();
+    getLatAndLongFromCityName(cityName);
+})
 
 function getLatAndLongFromCityName(cityname) {
-    return ["47.6769", "122.2060"];
+    var api_call = `http://api.openweathermap.org/geo/1.0/direct?q=${cityname}&limit=5&appid=${API_KEY}`
+    fetch(api_call)
+        .then((response) => response.json())
+        .then((data) => {console.log(data)});
+    
+    return [47.6062, 122.3321];   
 }
 
 function getWeatherDataFromApi(lat, lon) {
-    let api_call = CURRENT_WEATHER_ENDPOINT + "lat=" + lat + "lon=" + lon + "?key=" + API_KEY;
+    let api_call = FORECAST_ENDPOINT + "lat=" + lat + "&lon=" + lon + "&appid=" + API_KEY;
     console.log(api_call);
-    let api_response = fetch(api_call);
-    console.log(api_response);
+
+    fetch(api_call).then((response) => response.json())
+    .then((data) => {console.log(data)});
     return [];
 }
-console.log("hello");
+
+
 var latAndLong = getLatAndLongFromCityName(TEMP_CITY);
 getWeatherDataFromApi(latAndLong[0], latAndLong[1]);
