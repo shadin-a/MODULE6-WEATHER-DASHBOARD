e522c3ef3c57d43fa5dfee051cef0a57e30f1cb1
Shadin Al-Arab <shadin.alarab@gmail.com>
2022-08-29 08:20:09 -0700

    weather input in html added

--- script.js
+++ script.js
@@ -9,28 +9,52 @@ const SEARCH_BUTTON = document.getElementById("searchButton");
 
 $("#searchButton").click(function(event){
     event.preventDefault();
-    var cityName = TEMP_CITY.val();
-    getLatAndLongFromCityName(cityName);
+    var cityName = TEMP_CITY;
+    var cityLatandLong = getLatAndLongFromCityName(cityName);
+    var weatherData = getWeatherDataFromApi(cityLatandLong[0], cityLatandLong[1]);
+    console.log(weatherData.list[0]);
+    var todayWeather = weatherData.list[0];
+    var temp = todayWeather.main.temp;
+    var wind = todayWeather.wind.speed;
+    var humidity = todayWeather.main.humidity;
+
+    document.getElementById('currentTemp').innerText = temp;
+    document.getElementById('currentWind').innerText = wind;
+    document.getElementById('currentHumidity').innerText = humidity;
 })
 
 function getLatAndLongFromCityName(cityname) {
     var api_call = `http://api.openweathermap.org/geo/1.0/direct?q=${cityname}&limit=5&appid=${API_KEY}`
-    fetch(api_call)
-        .then((response) => response.json())
-        .then((data) => {console.log(data)});
-    
+    var latAndLong =  null;
+    $.ajax({
+        type: "GET",
+        url: api_call,
+        dataType: 'JSON',
+        async: false,
+        success: function(data){
+            latAndLong = data;
+        }
+    });
+    //return latandlong and format in nbs.
     return [47.6062, 122.3321];   
 }
 
 function getWeatherDataFromApi(lat, lon) {
     let api_call = FORECAST_ENDPOINT + "lat=" + lat + "&lon=" + lon + "&appid=" + API_KEY;
-    console.log(api_call);
+    var weather_data = null;
 
-    fetch(api_call).then((response) => response.json())
-    .then((data) => {console.log(data)});
-    return [];
+    $.ajax({
+        type: "GET",
+        url: api_call,
+        dataType: 'JSON',
+        async: false,
+        success: function(data){
+            weather_data = data;
+        }
+    });
+
+    return weather_data;
 }
 
-
-var latAndLong = getLatAndLongFromCityName(TEMP_CITY);
-getWeatherDataFromApi(latAndLong[0], latAndLong[1]);
+//var latAndLong = getLatAndLongFromCityName(TEMP_CITY);
+//getWeatherDataFromApi(latAndLong[0], latAndLong[1]);
