f3a7fdae574c64aac5cf60c956f55ce61046cbaf
Shadin Al-Arab <shadin.alarab@gmail.com>
2022-11-01 17:09:24 -0700

    lat and lon working

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
  // Replace me later with user input from form
 const API_KEY = "c2647d1123d8569a6d9fce9c4ea94398";
 const CURRENT_WEATHER_ENDPOINT = 'https://api.openweathermap.org/data/2.5/weather?';
+//https://api.openweathermap.org/data/2.5/onecall?lat=47.3075369&lon=-122.2301808&appid=f077831005b0a99879525b916f58d7b5&units=imperial
 const FORECAST_ENDPOINT = 'https://api.openweathermap.org/data/2.5/forecast?'
 const SEARCH_BUTTON = document.getElementById("searchButton");
-var cityLatAndLong =  null;
-// 1. Translate the city into latitude and longitude
-// 2. Send lat and long to api to get weather data back
+
 
 $("#searchButton").click(function(event){
     event.preventDefault();
@@ -14,61 +13,65 @@ $("#searchButton").click(function(event){
 
     // 1. Get lat and Long
     var cityLatandLong = getLatAndLongFromCityName(cityName);
-    console.log('lat and long: ', cityLatAndLong);
+    console.log('lat and long: ', cityLatandLong);
 
     // 2. Pass lat and long into api to get weather data
-    data = getWeatherDataFromApi(cityLatandLong);
+    // data = getWeatherDataFromApi(cityLatandLong);
 
     // 3. Render weather data on page
-    putDataInDivs(data);
+    //putDataInDivs(data);
 })
 
 function putDataInDivs(weatherData) {
-
+    get5DayForecast(weatherData.list)
     console.log(weatherData);
-    var todayWeather = weatherData
+    var todayWeather = weatherData;
+    console.log(todayWeather);
     var temp = todayWeather.main.temp;
     var wind = todayWeather.wind.speed;
     var humidity = todayWeather.main.humidity;
-    var icon = todayWeather.weather[0].icon;
-    document.getElementById('currentTemp').innerText = ('Temp: ' + temp);
-    document.getElementById('currentWind').append(wind);
-    document.getElementById('currentHumidity').append(humidity);
+    var icon = todayWeather.weather.icon;
+    document.getElementById('currentTemp').innerText = ('Temp: ' + temp + ' F');
+    document.getElementById('currentWind').innerText = ('Wind: ' + wind + ' MPH');
+    document.getElementById('currentHumidity').innerText = ('Humidity: ' + humidity + ' %');
+    document.getElementById('currentUVI').innerText = ('UVI' + UVI);
     document.getElementById('icon').innerText = icon;
 } 
 
 function getLatAndLongFromCityName(cityName) {
-    var api_call = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}`
+    var api_call = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=5&appid=${API_KEY}`
     const stuff = fetch(api_call) // using variable name to grab coordinates
     .then((response) => {
         return response.json();
     }).then((data) => {   
-        putDataInDivs(data)
+       getWeatherDataFromApi(data[0].lat, data[0].lon)
     });
 }
 function getWeatherDataFromApi(lat, lon) {
 
-    let api_call = FORECAST_ENDPOINT + "lat=" + lat + "&lon=" + lon + "&appid=" + API_KEY;
-    var weather_data = null;
+    let api_call = FORECAST_ENDPOINT + "lat=" + lat + "&lon=" + lon + "&appid=" + API_KEY + "&units=imperial";
+    var weather_data = [];
 
     fetch(api_call) // using variable name to grab coordinates
     .then((response) => {
-        console.log(response)
         if (response.ok){
         return response.json()};
     }).then((data) => {
-        console.log(data);
+        console.log(data)
+        putDataInDivs(data)
     })
-    return weather_data;
+   
 }
-function get5DayForecast (lat, lon) {
-    /*for (var i= 0; i < 5; i++) {
-        
-    }*/
+
+function get5DayForecast (array) {
+    for (var i= 0; i < 5; i++) {
+        console.log(array[i])
+    }
 }
 
 function saveCityInLocalStorage(cityName) {
-    let allCities = JSON.parse(localStorage.getItem(“all-cities”)) || [];
+    // let cityName = JSON.parse(localStorage.getItem(cityName)) || [
+    // ];
     //prompted when search
    // 
 }
